ad0b5d938f0f5b51d34f6476b75ed05343a094e6
jb <mail@jungbin.kim>
2018-05-07 17:20:13 +0900

    Complete gradient descent
    - Pass week2 ex1

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,8 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
+    temp0 = theta(1) - (alpha / m) * sum(X*theta - y);
+    temp1 = theta(2) - (alpha / m) * transpose(X*theta - y) * X(:, 2);
+    theta = [temp0;temp1];
 
 
 
