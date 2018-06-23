a50a12b2480264926141c8a1a545c18969d2cbe7
jb <mail@jungbin.kim>
2018-06-23 18:06:14 +0900

    Apply Selecting λ using a cross validation set

--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -39,14 +39,13 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
-
-
-
-
-
-
-
+for i = 1:length(lambda_vec)
+	lambda = lambda_vec(i);
+	theta_train = trainLinearReg(X, y, lambda);
+	error_train(i) = linearRegCostFunction(X, y, theta_train, 0);
+	% theta 값을 training set 의 값을 사용해야함
+	error_val(i) = linearRegCostFunction(Xval, yval, theta_train, 0);
+end
 
 % =========================================================================
 
