3b273072eccfceccdf71d3e09cb6bbab2ef4a1a2
jb <mail@jungbin.kim>
2018-06-30 17:33:00 +0900

    Find best C, sigma on Example Dataset 3

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,10 +23,34 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Test C, sigma
+%a = [0.01, 1, 30];
+a = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+a_length = size(a, 2);
 
+predictions_error = zeros(a_length ^2,1);
 
+x1 = X(:,1);
+x2 = X(:,2);
 
+for i = 1:a_length
+	for j = 1:a_length
+		c = a(i);
+		sigma = a(j);
+		model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
+		error = mean(double(predictions ~= yval));
+		predictions_error(a_length*(i-1) + j) = error;
+	end
+end
+
+% min error 값과 그 index 찾아 해당 C와 sigma 구함
+[min_error, min_index] = min(predictions_error);
+c_index = floor(min_index/a_length) + 1;
+sigma_index = mod(min_index, a_length);
+C = a(c_index);
+sigma = a(sigma_index);
 
 
 % =========================================================================
